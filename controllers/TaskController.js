7b9375cd503185d3bde0efb6eb513a1d792a1ece
nikola8217 <gileer404@gmail.com>
2022-05-20 14:50:48 +0200

    little fix

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,13 +1,13 @@
 const getAllTasks = (req, res) => {
-    res.send('all items from the file')
+    res.send('get all tasks')
 }
 
 const createTask = (req, res) => {
-    res.send('create task')
+    res.json(req.body)
 }
 
 const getTask = (req, res) => {
-    res.send('get single task')
+    res.json({id:req.params.id})
 }
 
 const updateTask = (req, res) => {
